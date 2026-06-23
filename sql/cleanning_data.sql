b56fafa01159dfe08895e26615acbea466f81f4f
Kiệt Nguyễn <nguyenhuukiet7a2@gmail.com>
2026-06-23 16:55:34 +0700

    feature: cleanning data

---
+++ sql/cleanning_data.sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT 
+    person_age, 
+    person_income, 
+    person_emp_length, 
+    loan_amnt, 
+    loan_int_rate, 
+    loan_status
+FROM db_credit_risk
+WHERE (person_age <= 100)
+  AND (person_emp_length <= person_age OR person_emp_length IS NULL)
+  AND (loan_amnt > 0)
+  AND (person_income > 0)
+  AND (loan_int_rate > 0 OR loan_int_rate IS NULL);

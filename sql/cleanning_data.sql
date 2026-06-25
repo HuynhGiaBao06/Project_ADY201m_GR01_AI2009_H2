80860cd344727cd32ca596ff347aa09f80aa15fa
Kiệt Nguyễn <nguyenhuukiet7a2@gmail.com>
2026-06-25 16:10:29 +0700

    change logic of cleanning data

--- sql/cleanning_data.sql
+++ sql/cleanning_data.sql
@@ -1,12 +1,24 @@
-SELECT DISTINCT 
-    person_age, 
-    person_income, 
-    person_emp_length, 
-    loan_amnt, 
-    loan_int_rate, 
-    loan_status
-FROM db_credit_risk
-WHERE (person_age <= 100)
-  AND (person_emp_length <= person_age OR person_emp_length IS NULL)
-  AND (loan_amnt > 0)
-  AND (person_income > 0);
+WITH MedianByGrade AS (
+    SELECT
+        loan_grade,
+        PERCENTILE_CONT(0.5)
+            WITHIN GROUP (ORDER BY loan_int_rate) AS median
+    FROM db_credit_risk
+    WHERE loan_int_rate IS NOT NULL
+    GROUP BY loan_grade
+)
+
+SELECT DISTINCT
+    d.person_age,
+    d.person_income,
+    COALESCE(d.person_emp_length, 1) AS person_emp_length,
+    d.loan_amnt,
+    COALESCE(d.loan_int_rate, m.median) AS loan_int_rate,
+    d.loan_status
+FROM db_credit_risk d
+LEFT JOIN MedianByGrade m
+    ON d.loan_grade = m.loan_grade
+WHERE d.person_age <= 100
+  AND (d.person_emp_length <= d.person_age OR d.person_emp_length IS NULL)
+  AND d.loan_amnt >= 0
+  AND d.person_income >= 0;

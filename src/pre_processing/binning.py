7b3a00b26aa629e09e27d7b1ea43b4b3dc712ae7
Dan <muichirou31@gmail.com>
2026-06-28 14:56:40 +0700

    update binning

--- src/pre_processing/binning.py
+++ src/pre_processing/binning.py
@@ -4,8 +4,49 @@ Rời rạc hóa (binning) biến độ tuổi person_age thành các khoảng c
 Giúp các mô hình thuật toán tuyến tính bắt được tính chu kỳ rủi ro theo từng nhóm tuổi vay vốn.
 """
 import pandas as pd
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
 
-class AgeBinner:
-    def __init__(self):
-        pass
-    
+class AgeBinner(BaseEstimator, TransformerMixin):
+    """
+    Chia tuổi thành các nhóm nhưng trả về mã số (ordinal bins)
+    để tương thích với StandardScaler và các mô hình ML.
+    """
+
+    def __init__(self,
+                 col_name='person_age',
+                 bin_size=7,
+                 start_age=14,
+                 end_age=119):
+        self.col_name = col_name
+        self.bin_size = bin_size
+        self.start_age = start_age
+        self.end_age = end_age
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_out = X.copy()
+
+        bins = np.arange(
+            self.start_age,
+            self.end_age + self.bin_size,
+            self.bin_size
+        )
+
+        age = X_out[self.col_name]
+
+        age = age.clip(lower=self.start_age, upper=self.end_age-1)
+        
+        X_out[self.col_name] = pd.cut(
+            age,
+            bins=bins,
+            labels=False,
+            include_lowest=True
+        )
+
+        X_out[self.col_name] = X_out[self.col_name].astype(float)
+
+        return X_out
+
